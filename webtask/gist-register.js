cef5c69ed0e1534b2617f311ffd787cfb35b7f04
Verlic Redclaw <verlic@live.com>
2015-09-27 00:42:39 -0300

    New webtasks: Register and User Gist integrations

---
+++ webtask/gist-register.js
@@ -0,0 +1,72 @@
+var request = require('request'),
+	mongoose = require('mongoose'),
+	userSchema = new mongoose.Schema({ slackId: String, gistToken: String });
+
+function checkDatabaseInitialized(context, callback) {
+	if (!this.dbInitialized) {
+		if (!this.GistUser) {
+			this.GistUser = mongoose.model('GistUser', userSchema);
+		}
+
+		mongoose.connect(context.data.GIST_CONNECTION);
+		this.db = mongoose.connection;
+		this.db.on('error', function (err) {
+			if (err) {
+				callback(err);
+			}
+
+			return;
+		});
+
+		this.db.once('open', function (cb) {
+			console.log('Database initialized.');
+			this.dbInitialized = true;
+		});
+	}
+}
+
+module.exports = function (context, callback) {
+	var slackId = context.data.user_id,
+		gistToken = context.data.text;	
+	
+	// Validate that incoming data is valid	
+	if (!slackId || !gistToken) {
+		callback('Unknown user or token not found');
+		return;
+	}
+
+	checkDatabaseInitialized(context, callback);
+	var data = { slackId: slackId, gistToken: gistToken };
+
+	this.GistUser.findOne({ slackId: slackId }, function (err, user) {
+		if (err) {
+			callback('Unable to retrieve gist user from the database');
+			return;
+		}
+
+		if (user) {
+			// Gist user found. Update its token
+			user.gistToken = gistToken;
+			user.slackId = slackId;
+			user.save(function (err, data) {
+				if (err) {
+					callback(err);
+					return;
+				}
+				
+				console.log(JSON.stringify(data));
+				callback(null, 'Gist token updated! Token: ' + gistToken);
+			});
+		} else {
+			var newUser = new this.GistUser(data);
+			newUser.save(function (err, data) {
+				if (err) {
+					callback(err);
+					return;
+				}
+
+				callback(null, 'Gist token registered! Token: ' + gistToken);
+			});
+		}
+	});
+}
--- webtask/gist-this.js
+++ webtask/gist-this.js
@@ -1,14 +1,116 @@
-var request = require('request');
+var request = require('request'),
+	mongoose = require('mongoose'),
+	GitHubApi = require('github@0.2.4'),
+	github = new GitHubApi({
+		// required
+		version: "3.0.0",
+		// optional
+		debug: true,
+		protocol: "https",
+		host: "api.github.com", // should be api.github.com for GitHub
+		pathPrefix: "", // for some GHEs; none for GitHub
+		timeout: 5000,
+		headers: {
+			"user-agent": "My-Cool-GitHub-App" // GitHub is happy with a unique user agent
+		}
+	}),
+	userSchema = new mongoose.Schema({ slackId: String, gistToken: String });
+
+function notifySlack(url, gist, callback){
+	// Gist created successfully. Now, let's notify Slack using an Incoming WebHook.	
+	var payload = {
+		attachments: [{
+			fallback: 'Take a look at this Gist. ' + gist.filename + ': <' + url + '>',
+			pretext: 'Take a look at this Gist. ' + gist.filename + ': <' + url + '>',
+			color: 'good',
+			fields: [{ title: gist.filename, value: gist.file[gist.filename].content, short: false }]
+		}]
+	};
+
+	// If a source channel is specified, send the response back to that channel
+	if (gist.sourceChannel && gist.sourceChannel !== 'directmessage') {
+		console.log('Slack channel found: ' + gist.sourceChannel);				
+		payload.channel = '#' + gist.sourceChannel;
+	}
+
+	// Invoke the Slack Incoming WebHook
+	request.post({ url: gist.slackReplyUrl, form: { payload: JSON.stringify(payload) } }, function (error, response, body) {
+		if (error) {
+			callback('Unable to send reply back to Slack.');
+		}
+
+		callback(null, 'Anonymous Gist created! ' + gist.filename + ': <' + url + '>');
+	});
+}
+
+function createAnonymousGist(gist, callback){
+	request.post({
+		headers: { 'User-Agent': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)' },
+		url: 'https://api.github.com/gists',
+		json: {	description: 'Generated gist from Slack',
+			public: 'false',
+			files: gist.file
+		}
+	}, function (error, response, body) {
+		if (error) {
+			callback('Error while creating gist:' + JSON.stringify(error));
+		} else if (!body) {
+			callback('Unable to parse body from Gist');
+		} else {
+			notifySlack(body.html_url, gist, callback);
+		}
+	});
+}
+
+function createUserGist(gistUser, gist, callback){
+	console.log('Gist User: ' + JSON.stringify(gistUser));
+	github.authenticate({
+		type: 'oauth',
+		token: gistUser.gistToken
+	});
+	
+	var payload = {	
+		description: 'Generated gist from Slack',
+		public: 'false',
+		files: gist.file
+	};
+	
+	github.gists.create(payload, function(err, body){
+		if (err) {
+				callback('Error while creating gist:' + JSON.stringify(err));
+			} else if (!body) {
+				callback('Unable to parse body from Gist');
+		}
+		
+		console.log('Gist created: ' + JSON.stringify(body));		
+		notifySlack(body.html_url, gist, callback);
+	});
+}
+
+function findGistUser(slackId, cb){
+	this.GistUser.findOne({ slackId: slackId }, function (err, user) {
+		if (err) {
+			cb('Unable to retrieve gist user from the database');
+			return;
+		}
+
+		cb(null, user);		
+	});
+}
+
 
 module.exports = function (context, callback) {
 	var incomingToken = context.data.token,
-		slackToken = context.data.SLACK_TOKEN,
-		slackReplyUrl = context.data.SLACK_URL,
-		sourceChannel = context.data.channel_name,
 		command = context.data.text,
-		filename = command && command.split(' ')[0] || '',		
-		file = {};
-	
+		slackToken = context.data.SLACK_TOKEN,
+		gist = {
+			slackId: context.data.user_id,			
+			slackReplyUrl: context.data.SLACK_URL,
+			sourceChannel: context.data.channel_name,
+			file: {},
+			filename: command && command.split(' ')[0] || '' 
+		};
+		
 	// First we validate that the request comes from our Slack Command	
 	if (incomingToken !== slackToken) {
 		callback('Invalid token. Unauthorized.');
@@ -22,48 +124,36 @@ module.exports = function (context, callback) {
 	}
 	
 	// Create the gist file object
-	file[filename] = { content: command.replace(filename, '').trim()};
-	console.log('Gist Data: ' + JSON.stringify(file[filename]));
+	gist.file[gist.filename] = { content: command.replace(gist.filename, '').trim()};
+	console.log('Gist Data: ' + JSON.stringify(gist.file[gist.filename]));
 
-	// Call Gist API to create a new gist
-	request.post({
-		headers: { 'User-Agent': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)' },
-		url: 'https://api.github.com/gists',
-		json: {	description: 'Generated gist from Slack',
-			public: 'false',
-			files: file
+	if (!this.dbInitialized) {
+		if (!this.GistUser) {
+			this.GistUser = mongoose.model('GistUser', userSchema);
 		}
-	}, function (error, response, body) {
-		if (error) {
-			callback(error);
-		} else if (!body) {
-			callback('Unable to parse body from Gist');
-		} else {
-			// Gist created successfully. Now, let's notify Slack using an Incoming WebHook.
-			var url = body.html_url;
-			var payload = {
-				attachments: [{
-					fallback: 'Take a look at this Gist. ' + filename + ': <' + url + '>',
-					pretext: 'Take a look at this Gist. ' + filename + ': <' + url + '>',
-					color: 'good',
-					fields: [{ title: filename, value: file[filename].content, short: false }]
-				}]
-			};
-
-			// If a source channel is specified, send the response back to that channel
-			if (sourceChannel && sourceChannel !== 'directmessage') {
-				console.log('Slack channel found: ' + sourceChannel);				
-				payload.channel = '#' + sourceChannel;
+		
+		mongoose.connect(context.data.GIST_CONNECTION);
+		this.db = mongoose.connection;
+		this.db.on('error', function (err) {
+			if (err) {
+				callback(err);
 			}
 
-			// Invoke the Slack Incoming WebHook
-			request.post({ url: slackReplyUrl, form: { payload: JSON.stringify(payload) } }, function (error, response, body) {
-				if (error) {
-					callback('Unable to send reply back to Slack.');
-				}
+			return;
+		});
 
-				callback(null, 'Gist created! ' + filename + ': <' + url + '>');
-			});
+		this.db.once('open', function (cb) {
+			console.log('Database initialized.');
+			this.dbInitialized = true;
+		});
+	}
+	
+	findGistUser(gist.slackId, function(err, gistUser){
+		if (!gistUser){
+			// Create an anonymous gist
+			createAnonymousGist(gist, callback);
+		}else{
+			createUserGist(gistUser, gist, callback);
 		}
 	});
 }
